bd5215796ee8f31fc698083c91466ce7b81db328
Egor Wexler <ferotex7@gmail.com>
2020-09-15 12:35:44 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from APISureConnector.apisureconnector import APISureConnector
+import json
+from datetime import datetime
+from pprint import pp
+
+if __name__ == '__main__':
+    connector = APISureConnector(client_id="gAtxkHbIAwDOHnSTajn0p0tN4V6Yhk1B",
+                                 client_secret="80OdKmebSkWlpkGP")
+
+    url = 'https://api.apisure.io/mapi_base/v1/Guarantee/Guarantee/Apply'
+    filename = 'guarantee_sample.json'
+
+    with open(filename) as file:
+        data = json.load(file)
+
+    data["GuaranteeApplicationDetails"]["PartyReference"]["ApplicantReference"] = datetime.now().strftime("%m%d%Y")
+
+    http_response, json_data = connector.send_request(url=url, data=data)
+
+    print(http_response)
+    pp(json_data)
